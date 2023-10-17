ce6c299881beda08a63b9d4e786ef2ebed4945d8
SebastianAlfonsoMartinez <sebastianalfonso94@gmail.com>
2023-10-17 17:25:44 -0500

    Ejercicio con operadores matematicos en Java

---
+++ example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int num1 = 8;
+        int num2 = 2;
+
+        //Operadores matematicos
+        System.out.println("Operadores matematicos");
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + ((double)num1 / num2));
+        System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
+
+    }
+}
